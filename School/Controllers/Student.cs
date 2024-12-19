f2da62a6e6ca0296283bc379bb00effb57b961a9
prabh <prabh@LAPTOP-SHSMVAML>
2024-12-19 12:12:10 -0500

    Add project files.

---
+++ School/Controllers/Student.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using School.Models;
+using System.Collections.Generic;
+
+namespace StudentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        
+        private static List<Student> students = new List<Student>
+        {
+            new Student { StudentId = 1, FirstName = "Johnny", LastName = "Storm", Age = 22, Email = "fire@hotmail.com" },
+            new Student { StudentId = 2, FirstName = "Harry", LastName = "Osborn", Age = 22, Email = "goblin@yahoo.com" },
+            new Student { StudentId = 3, FirstName = "Peter", LastName = "Parker", Age = 22, Email = "spider@gmail.com" }
+        };
+
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> GetStudents()
+        {
+            return Ok(students);
+        }
+    }
+}
